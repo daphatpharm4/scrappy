7861d7f6690aa16a7a2087511d02d511f77c1760
daphatpharm4 <53215642+daphatpharm4@users.noreply.github.com>
2025-12-24 01:53:23 +0100

    Add GitHub Actions deploy pipeline and remote backend

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "azurerm" {
+    resource_group_name  = "adl-dev-rg"
+    storage_account_name = "adltfstatesa"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
