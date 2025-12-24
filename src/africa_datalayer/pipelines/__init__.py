b7f8ecbe73b7bd34e806047a7aef1602bffcb197
daphatpharm4 <53215642+daphatpharm4@users.noreply.github.com>
2025-12-24 01:36:38 +0100

    Delete src/africa_datalayer directory

--- src/africa_datalayer/pipelines/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Pipeline entrypoints for ingestion and cleansing flows."""
-
-from .cleanse import cleanse_from_raw
-from .ingest import ingest_dataset
-
-__all__ = ["cleanse_from_raw", "ingest_dataset"]
